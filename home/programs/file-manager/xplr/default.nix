dad336bd74ca5dd988383db5409f4937a796a2b7
heisfer <heisfer@refract.dev>
2023-10-23 23:21:43 +0300

    trying xplr plugins with git submodules :)

--- home/programs/file-manager/xplr/default.nix
+++ home/programs/file-manager/xplr/default.nix
@@ -7,8 +7,20 @@
     enable = true;
     extraConfig =
     ''
+    local home = os.getenv("HOME")
+    package.path = home
+    .. "/.config/xplr/plugins/?/init.lua;"
+    .. home
+    .. "/.config/xplr/plugins/?.lua;"
+    .. package.path
+    require("zentable").setup()
+    require("tree-view").setup()
+
+
     xplr.config.modes.builtin.default.key_bindings.on_key["e"] =
-      xplr.config.modes.builtin.action.key_bindings.on_key["e"]
+    xplr.config.modes.builtin.action.key_bindings.on_key["e"]
     '';
   };
+
+  xdg.configFile."xplr/plugins".source = ./plugins;
 }
