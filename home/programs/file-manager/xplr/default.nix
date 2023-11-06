d73d6af6c4da4774c07a9b5e7d58b7dcff379332
heisfer <heisfer@refract.dev>
2023-11-06 09:07:07 +0200

    format

--- home/programs/file-manager/xplr/default.nix
+++ home/programs/file-manager/xplr/default.nix
@@ -1,23 +1,18 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{...}: {
   programs.xplr = {
     enable = true;
-    extraConfig =
-    ''
-    local home = os.getenv("HOME")
-    package.path = home
-    .. "/.config/xplr/plugins/?/init.lua;"
-    .. home
-    .. "/.config/xplr/plugins/?.lua;"
-    .. package.path
-    require("tree-view").setup()
-    require("icons").setup()
+    extraConfig = ''
+      local home = os.getenv("HOME")
+      package.path = home
+      .. "/.config/xplr/plugins/?/init.lua;"
+      .. home
+      .. "/.config/xplr/plugins/?.lua;"
+      .. package.path
+      require("tree-view").setup()
+      require("icons").setup()
 
-    xplr.config.modes.builtin.default.key_bindings.on_key["e"] =
-    xplr.config.modes.builtin.action.key_bindings.on_key["e"]
+      xplr.config.modes.builtin.default.key_bindings.on_key["e"] =
+      xplr.config.modes.builtin.action.key_bindings.on_key["e"]
     '';
   };
 
