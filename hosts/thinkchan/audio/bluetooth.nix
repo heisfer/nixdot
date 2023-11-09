3ac5e40c71bc0b0ac5f90b2d60a3a0d736d734bf
heisfer <heisfer@refract.dev>
2023-11-09 15:02:03 +0200

    added bluetooth media controls

--- hosts/thinkchan/audio/bluetooth.nix
+++ hosts/thinkchan/audio/bluetooth.nix
@@ -1,8 +1,12 @@
-{ ... }:
-{
+{pkgs, ...}: {
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
-    
+  };
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = ["network.target" "sound.target"];
+    wantedBy = ["default.target"];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
   };
 }
