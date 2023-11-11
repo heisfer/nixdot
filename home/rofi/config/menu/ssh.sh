171b493f3ab3cfea590e8b38a288cedc85bbc377
heisfer <heisfer@refract.dev>
2023-11-11 16:44:48 +0200

    Removed rofi menu and split menus to keybindings

--- home/rofi/config/menu/ssh.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-options="username@host"
-
-selection=$(echo -e "${options}" | rofi -dmenu -p "Menu") 
-wezterm ssh $selection
--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,13 +1,9 @@
 {pkgs, ...}: {
-    programs.rofi = {
-        enable = true;
-        font = "Comic Code Ligatures 14";
-        package = pkgs.rofi-wayland;
-        plugins = with pkgs; [
-            rofi-calc
-            rofi-bluetooth
-            rofi-rbw
-        ];
-    };
-    xdg.configFile."rofi".source = ./config;
+  programs.rofi = {
+    enable = true;
+#    font = "Comic Code Ligatures 14";
+    package = pkgs.rofi-wayland;
+    theme = ./ayu.rasi;
+  };
+#  xdg.configFile."rofi".source = ./config;
 }
