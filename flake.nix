f452c278281547971eabec9628bfbdd0ff813bba
heisfer <heisfer@refract.dev>
2023-11-12 13:38:54 +0200

    format flake.nix

--- flake.nix
+++ flake.nix
@@ -17,12 +17,11 @@
     };
 
     helix.url = "github:helix-editor/helix";
-    
+
     rycee-nurpkgs = {
       url = gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons;
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
 
     kmonad = {
       url = "github:kmonad/kmonad?dir=nix";
@@ -31,20 +30,18 @@
 
     nurpkgs.url = github:nix-community/NUR;
   };
-  outputs = inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = import ./overlays { inherit inputs system; };
-      };
-      extraArgs = {
-        inherit (inputs.rycee-nurpkgs.lib.${system}) buildFirefoxXpiAddon;
-        addons = pkgs.nur.repos.rycee.firefox-addons;
-      };
-    in
-    {
-      nixosConfigurations =
-        import ./hosts { inherit inputs system pkgs extraArgs; };
-      };
-    }
+  outputs = inputs: let
+    system = "x86_64-linux";
+    pkgs = import inputs.nixpkgs {
+      inherit system;
+      overlays = import ./overlays {inherit inputs system;};
+    };
+    extraArgs = {
+      inherit (inputs.rycee-nurpkgs.lib.${system}) buildFirefoxXpiAddon;
+      addons = pkgs.nur.repos.rycee.firefox-addons;
+    };
+  in {
+    nixosConfigurations =
+      import ./hosts {inherit inputs system pkgs extraArgs;};
+  };
+}
