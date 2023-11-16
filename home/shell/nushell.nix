32fd4fc949454935afc44812543e736ef291c0c5
heisfer <heisfer@refract.dev>
2023-11-16 22:12:46 +0200

    added stuff to nushell

--- home/shell/nushell.nix
+++ home/shell/nushell.nix
@@ -2,5 +2,19 @@
 {
   programs.nushell = {
     enable = true;
+	extraConfig = ''
+	  $env.config = {
+	    show_banner: false,
+	    completions: {
+	      case_sensitive: false
+	      quick: true
+	      partial: true
+	      algorithm: prefix
+	      external: {
+	        enable: true
+		  }
+	    }
+	  }
+	'';
   };
 }
