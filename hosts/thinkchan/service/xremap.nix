841b95f2f23f71392dbec52d0b3c3b2d03d7aaa2
heisfer <heisfer@refract.dev>
2023-12-04 03:28:04 +0200

    v3

--- hosts/thinkchan/service/xremap.nix
+++ hosts/thinkchan/service/xremap.nix
@@ -1,16 +1,28 @@
 {
   services.xremap = {
-    serviceMode = "user";
-    withWlroots = true;
+    withHypr = true;
     userName = "heisfer";
-    config = {
-      keymap = [
-        {
-          remap = {
-            C-h = "Backspace";
-          };
-        }
-      ];
-    };
+    serviceMode = "user";
+    deviceName = "AT Translated Set 2 keyboard";
+    yamlConfig = ''
+      virtual_modifiers:
+        - CapsLock
+      keymap:
+        - name: Global
+          remap:            
+
+            # Programmer-colemak layout for third layer
+            CAPSLOCK-A: LEFTBRACE
+            CAPSLOCK-S: SHIFT-LEFTBRACE
+            CAPSLOCK-D: EQUAL
+            CAPSLOCK-F: SHIFT-9
+            CAPSLOCK-G: SHIFT-8
+            CAPSLOCK-H: MINUS
+            CAPSLOCK-J: SHIFT-0
+            CAPSLOCK-K: SHIFT-MINUS
+            CAPSLOCK-L: SHIFT-RIGHTBRACE
+            CAPSLOCK-SEMICOLON: RIGHTBRACE
+            
+    '';
   };
 }
