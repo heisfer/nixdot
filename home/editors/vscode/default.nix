eb9e71c9f20cb19520f5a027a7f6c60dc1500c38
heisfer <heisfer@refract.dev>
2023-12-04 03:26:20 +0200

    #1 fixed vscode, need more extensions later

--- home/editors/vscode/default.nix
+++ home/editors/vscode/default.nix
@@ -1,5 +1,6 @@
 {
   pkgs
+, extraArgs
 ,  ...
 }: {
  
@@ -7,5 +8,30 @@
     enable = true;
     package = pkgs.vscodium;
     enableUpdateCheck = false;
+    extensions = with extraArgs.vscode-extensions.vscode-marketplace; [
+      teabyii.ayu
+      nullxception.cherry-theme
+      gulajavaministudio.mayukaithemevsc
+      #php
+      devsense.phptools-vscode
+
+      #nix
+      jnoortheen.nix-ide
+      arrterian.nix-env-selector
+      mkhl.direnv
+
+      #dart
+      dart-code.flutter
+      dart-code.dart-code
+      #AI?
+      codeium.codeium
+    ];
+    userSettings = {
+      #UI
+      "workbench.colorTheme" = "Cherry Midnight";
+      "window.titleBarStyle" = "custom";
+      "editor.fontFamily" = "Comic Code Ligatures";
+      
+    };
   };
 }
