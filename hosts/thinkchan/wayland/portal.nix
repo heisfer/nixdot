f94988c98810c4f57a733b6a967fb51a4e8f41d8
heisfer <heisfer@refract.dev>
2023-12-14 03:27:16 +0200

    NixOS: updated portal up to 22.11

--- hosts/thinkchan/wayland/portal.nix
+++ hosts/thinkchan/wayland/portal.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
-  xdg.portal = {
-    enable = true;
-	extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  };
+      xdg.portal = {
+        enable = true;
+        extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-hyprland ];
+        configPackages = [ pkgs.xdg-desktop-portal-hyprland ]; # needed from 23.11
+      };
+
 }
