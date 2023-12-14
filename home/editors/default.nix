95c50cee74f4a3b37df70f4ea018f9aba3c092e3
heisfer <heisfer@refract.dev>
2023-12-14 04:58:07 +0200

    Home: Editors -> added more lsp

--- home/editors/default.nix
+++ home/editors/default.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   imports = [
     ./helix
     ./neovim
@@ -9,16 +8,20 @@
   #lsp
   home.packages = with pkgs; [
     luajitPackages.lua-lsp
-    rnix-lsp
-    phpactor
+    lua-language-server
+    nil
+    nixd
+    nixpkgs-fmt
+    nodePackages_latest.bash-language-server
     nodePackages_latest.intelephense
     nodePackages_latest.typescript-language-server
     nodePackages_latest.vscode-css-languageserver-bin
     nodePackages_latest.vscode-html-languageserver-bin
     nodePackages_latest.vscode-json-languageserver
-    nodePackages_latest.bash-language-server
-    lua-language-server
+    nodePackages_latest.vscode-json-languageserver
+    phpactor
+    rnix-lsp
+    vala-language-server
     yaml-language-server
-    nil
   ];
 }
