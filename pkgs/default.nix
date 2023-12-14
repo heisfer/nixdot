8d4f673d033e3e2798bcbfad14d43bd822288f76
heisfer <heisfer@refract.dev>
2023-12-14 03:17:51 +0200

    LocalPkgs: updated list

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,6 @@
   wezterm = pkgs.callPackage ./wezterm {};
   wavebox = pkgs.callPackage ./wavebox {};
   spacedrive = pkgs.callPackage ./spacedrive {};
+  sigmafm = pkgs.callPackage ./sigmafm {};
+  swaylock-fprintd = pkgs.callPackage ./swaylock-fprintd {};
 }
