f5788a79dadb61e0eb01554b3860cf450296af12
heisfer <heisfer@refract.dev>
2024-01-05 19:50:53 +0200

    nixos/greetd: fixed style

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -1,21 +1,33 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    # theme packages
-    rose-pine-gtk-theme
-    bibata-cursors
-  ];
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  users.extraUsers.greeter = {
+    packages = with pkgs; [
+      inputs.self.packages.${pkgs.system}.rose-pine-gtk
+      bibata-cursors
+    ];
+    # For caching and such
+    home = "/tmp/greeter-home";
+    createHome = true;
+  };
 
   programs.regreet = {
     enable = true;
 
-    # cageArgs = ["-s" "-m" "last"];
+    cageArgs = ["-s" "-m" "last"];
 
     settings = {
+      background = {
+        path = ./wallpaper.jpg;
+        fit = "Cover";
+      };
       GTK = {
-        application_prefer_dark_theme = true;
+        # application_prefer_dark_theme = true;
         cursor_theme_name = "Bibata-Modern-Classic";
         icon_theme_name = "Adwaita";
-        theme_name = "rose-pine";
+        theme_name = "RosePine-Main-B-LB";
       };
     };
   };
