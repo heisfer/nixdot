18be02d2f28a82ae1c0c75a91826000281618710
heisfer <heisfer@refract.dev>
2024-01-30 10:59:12 +0200

    Home/Internet: Syntax error fix

--- home/internet/firefox/default.nix
+++ home/internet/firefox/default.nix
@@ -48,71 +48,69 @@ in {
     };
 
     profiles.dev-edition-default = {
-        id = 0;
-        inherit extensions;
+      id = 0;
+      inherit extensions;
 
-        search = {
-          force = true;
-          engines = {
-            "Bing".metaData.hidden = true;
-            "Wikipedia (en)".metaData.hidden = true;
-            "Amazon.com".metaData.hidden = true;
+      search = {
+        force = true;
+        engines = {
+          "Bing".metaData.hidden = true;
+          "Wikipedia (en)".metaData.hidden = true;
+          "Amazon.com".metaData.hidden = true;
 
-            "Nix Packages" = {
-              urls = [
-                {
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    {
-                      name = "type";
-                      value = "packages";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
+          "Nix Packages" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
 
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = ["@np"];
-            };
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@np"];
+          };
 
-            "NixOS Wiki" = {
-              urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-              iconUpdateURL = "https://nixos.wiki/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # every day
-              definedAliases = ["@nw"];
-            };
+          "NixOS Wiki" = {
+            urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+            iconUpdateURL = "https://nixos.wiki/favicon.png";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = ["@nw"];
+          };
 
-            "Home-Manager" = {
-              urls = [{template = "https://mipmip.github.io/home-manager-option-search/?query={searchTerms}";}];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = ["@hm"];
-            };
-
+          "Home-Manager" = {
+            urls = [{template = "https://mipmip.github.io/home-manager-option-search/?query={searchTerms}";}];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@hm"];
           };
         };
-        userChrome = ''
-          @import 'cascade/cascade-config.css';
-          @import 'cascade/cascade-layout.css';
-          @import 'cascade/cascade-responsive.css';
-          @import 'cascade/cascade-floating-panel.css';
-          @import 'cascade/cascade-nav-bar.css';
-          @import 'cascade/cascade-tabs.css';
-          @import 'cascade-tcr.css';
-          @import 'cascade-rose-pine.css';
+      };
+      userChrome = ''
+        @import 'cascade/cascade-config.css';
+        @import 'cascade/cascade-layout.css';
+        @import 'cascade/cascade-responsive.css';
+        @import 'cascade/cascade-floating-panel.css';
+        @import 'cascade/cascade-nav-bar.css';
+        @import 'cascade/cascade-tabs.css';
+        @import 'cascade-tcr.css';
+        @import 'cascade-rose-pine.css';
 
-        '';
-        settings = {
-          "browser.fullscreen.autohide" = false;
-          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
-          "extensions.pocket.enabled" = false;
-          "extensions.pocket.showHome" = false;
-          "signon.rememberSignons" = false;
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        };
+      '';
+      settings = {
+        "browser.fullscreen.autohide" = false;
+        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+        "extensions.pocket.enabled" = false;
+        "extensions.pocket.showHome" = false;
+        "signon.rememberSignons" = false;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
     };
   };
