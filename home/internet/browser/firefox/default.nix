dbda4590a7a7d6ca5892563c3dc8be130f4b4a28
heisfer <heisfer@aol.com>
2024-05-10 01:03:56 +0300

    Firefox: fixed @hm search, @np swtiched to unstable by default

--- home/internet/browser/firefox/default.nix
+++ home/internet/browser/firefox/default.nix
@@ -1,12 +1,12 @@
+{ pkgs, ... }:
 {
-  pkgs,
-  inputs,
-  ...
-}: {
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-devedition.override {
-      nativeMessagingHosts = [pkgs.web-eid-app pkgs.keepassxc];
+      nativeMessagingHosts = [
+        pkgs.web-eid-app
+        pkgs.keepassxc
+      ];
       extraPolicies.SecurityDevices.p11-kit-proxy = "${pkgs.p11-kit}/lib/p11-kit-proxy.so";
     };
     profiles.dev-edition-default = {
@@ -25,12 +25,8 @@
           "Wikipedia (en)".metaData.hidden = true;
           "Amazon.com".metaData.hidden = true;
           "Youtube  Music" = {
-            urls = [
-              {
-                template = "https://music.youtube.com/search?q={searchTerms}";
-              }
-            ];
-            definedAliases = ["ym"];
+            urls = [ { template = "https://music.youtube.com/search?q={searchTerms}"; } ];
+            definedAliases = [ "ym" ];
           };
           "Nix Packages" = {
             urls = [
@@ -42,6 +38,10 @@
                     value = "packages";
                   }
                   {
+                    name = "channel";
+                    value = "unstable";
+                  }
+                  {
                     name = "query";
                     value = "{searchTerms}";
                   }
@@ -50,20 +50,34 @@
             ];
 
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@np"];
+            definedAliases = [ "@np" ];
           };
 
           "NixOS Wiki" = {
-            urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+            urls = [ { template = "https://nixos.wiki/index.php?search={searchTerms}"; } ];
             iconUpdateURL = "https://nixos.wiki/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["@nw"];
+            definedAliases = [ "@nw" ];
           };
 
           "Home-Manager" = {
-            urls = [{template = "https://mipmip.github.io/home-manager-option-search/?query={searchTerms}";}];
+            urls = [
+              {
+                template = "https://home-manager-options.extranix.com/";
+                params = [
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                  {
+                    name = "release";
+                    value = "master";
+                  }
+                ];
+              }
+            ];
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@hm"];
+            definedAliases = [ "@hm" ];
           };
         };
       };
@@ -72,8 +86,7 @@
         "extensions.pocket.enabled" = false; # Disables firefox pocket
         "full-screen-api.ignore-widgets" = true; # Make videos full screen without being fullscreen
         "browser.tabs.closeWindowWithLastTab" = false; # Make so the browser doesn't close when last tab is closed
-        "identity.fxaccounts.enabled" = false; #Disable Firefox accounts integration.
-
+        "identity.fxaccounts.enabled" = false; # Disable Firefox accounts integration.
       };
     };
   };
