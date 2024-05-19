73f46bca942660ef3d14a9a4a9b557177a338dc6
heisfer <heisfer@aol.com>
2024-05-19 21:58:47 +0300

    Home/Foot: Made 'Pine' Color brither

--- home/terminal/foot/default.nix
+++ home/terminal/foot/default.nix
@@ -19,7 +19,7 @@
         foreground = "e0def4";
         regular0 = "26233a"; # black (Overlay)
         regular1 = "eb6f92"; # red (Love)
-        regular2 = "31748f"; # green (Pine)
+        regular2 = "3e8fb0"; # green (Pine)
         regular3 = "f6c177"; # yellow (Gold)
         regular4 = "9ccfd8"; # blue (Foam)
         regular5 = "c4a7e7"; # magenta (Iris)
@@ -28,7 +28,7 @@
 
         bright0 = "6e6a86"; # bright black (Overlay)
         bright1 = "eb6f92"; # bright red (Love)
-        bright2 = "31748f"; # bright green (Pine)
+        bright2 = "3e8fb0"; # bright green (Pine)
         bright3 = "f6c177"; # bright yellow (Gold)
         bright4 = "9ccfd8"; # bright blue (Foam)
         bright5 = "c4a7e7"; # bright magenta (Iris)
