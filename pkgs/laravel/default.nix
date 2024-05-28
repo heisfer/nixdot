5512df6bdd84a4535388dc97a3a66e6d73731a56
heisfer <heisfer@refract.dev>
2024-05-28 16:06:04 +0300

    pkgs/laravel: composer wrapper test

--- pkgs/laravel/default.nix
+++ pkgs/laravel/default.nix
@@ -1,4 +1,9 @@
-{ php, fetchFromGitHub, ... }:
+{
+  lib,
+  fetchFromGitHub,
+  makeWrapper,
+  php,
+}:
 php.buildComposerProject (finalAttrs: {
   pname = "laravel";
   version = "5.8.1";
@@ -10,6 +15,22 @@ php.buildComposerProject (finalAttrs: {
     hash = "sha256-LQABZnmKgJ8qkymmSjhjc+x1qZ/tFqFyQbfeGZECxok=";
   };
 
+  nativeBuildInputs = [ makeWrapper ];
+
   composerLock = ./composer.lock;
   vendorHash = "sha256-f18N2qNCUFetCaHaC4X6Benq70x21SVQ3YSs8kovK1g=";
+
+  postInstall = ''
+    wrapProgram $out/bin/laravel \
+      --suffix PATH : ${lib.makeBinPath [ php.packages.composer ]}
+  '';
+
+  meta = {
+    description = "The Laravel application installer.";
+    homepage = "https://laravel.com/docs#creating-a-laravel-project";
+    changelog = "https://github.com/laravel/installer/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    mainProgram = "laravel";
+    maintainers = with lib.maintainers; [ heisfer ];
+  };
 })
