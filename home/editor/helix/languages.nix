bb279d6dc43eb8bd11c0fe985922b77c75fb3f0a
heisfer <heisfer@refract.dev>
2024-06-04 02:50:09 +0300

    home/helix: removed string conversion

--- home/editor/helix/languages.nix
+++ home/editor/helix/languages.nix
@@ -8,12 +8,12 @@
   programs.helix.languages = {
     language-server = {
       nil = {
-        command = "${lib.getExe inputs.nil.packages.${pkgs.system}.default}";
+        command = lib.getExe inputs.nil.packages.${pkgs.system}.default;
         config.nil.formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
       };
 
       vscode-html-language-server = {
-        command = "${lib.getExe' pkgs.vscode-langservers-extracted "vscode-html-language-server"}";
+        command = lib.getExe' pkgs.vscode-langservers-extracted "vscode-html-language-server";
         args = [ "--stdio" ];
         config = {
           provideFormatter = true;
@@ -21,33 +21,33 @@
       };
 
       tsserver = {
-        command = "${lib.getExe' pkgs.nodePackages_latest.typescript-language-server "typescript-language-server"}";
+        command = lib.getExe' pkgs.nodePackages_latest.typescript-language-server "typescript-language-server";
         args = [ "--stdio" ];
       };
 
       # don't need to specify bash in languages because I'm only rewriting lsp command
       bash-language-server = {
-        command = "${lib.getExe pkgs.nodePackages_latest.bash-language-server}";
+        command = lib.getExe pkgs.nodePackages_latest.bash-language-server;
         args = [ "start" ];
       };
 
       emmet-lsp = {
-        command = "${lib.getExe pkgs.emmet-language-server}";
+        command = lib.getExe pkgs.emmet-language-server;
         args = [ "--stdio" ];
       };
 
       intelephense = {
-        command = "${lib.getExe' pkgs.nodePackages_latest.intelephense "intelephense"}";
+        command = lib.getExe' pkgs.nodePackages_latest.intelephense "intelephense";
         args = [ "--stdio" ];
       };
 
       phpactor = {
-        command = "${lib.getExe' pkgs.phpactor "phpactor"}";
+        command = lib.getExe' pkgs.phpactor "phpactor";
         args = [ "language-server" ];
       };
 
       tailwindcss-ls = {
-        command = "${lib.getExe pkgs.tailwindcss-language-server}";
+        command = lib.getExe pkgs.tailwindcss-language-server;
         args = [ "--stdio" ];
       };
     };
@@ -143,7 +143,6 @@
             "vscode-html-language-server"
             "emmet-lsp"
           ];
-          # language-servers = [ "gopls" ];
         }
       ];
     grammar = [
