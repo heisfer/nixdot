8f6de0fc3d2ccc03dfff7db9753f97635cbe2d03
heisfer <heisfer@refract.dev>
2024-06-14 18:03:42 +0300

    home/vesktop: added some plugins

--- home/programs/vesktop/default.nix
+++ home/programs/vesktop/default.nix
@@ -4,9 +4,18 @@
     # same as veskord/settings/settings.json
     settings = {
       autoUpdate = false;
+      autoUpdateNotification = false;
+      notifyAboutUpdates = false;
       enabledThemes = [ "theme.css" ];
       plugins = {
-        AlwaysAnimate.enabled = true;
+        NoTrack.enabled = true;
+        Settings.enabled = true;
+        WebContextMenus.enabled = true;
+        WhoReacted.enabled = true;
+        Translate.enabled = true;
+        BetterFolders.enabled = true;
+        ImageZoom.enabled = true;
+        FakeNitro.enabled = true;
       };
     };
     theme = ./rose-pine.theme.css;
