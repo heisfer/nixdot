cc21d87eed01867b73f41ba822bf64927293d8e7
heisfer <heisfer@refract.dev>
2024-06-16 22:48:58 +0300

    nixos/voyage: amd stuff

--- nixos/voyage/hardware-configuration.nix
+++ nixos/voyage/hardware-configuration.nix
@@ -48,5 +48,8 @@
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd = {
+    updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    ryzen-smu.enable = true;
+  };
 }
