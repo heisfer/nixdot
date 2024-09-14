4c3d5c773d3bef55ffb83729d07969c4d0390db4
heisfer <heisfer@refract.dev>
2024-09-14 14:06:05 +0300

    Home/GTK: Fixed extraConfig dark theme preference

--- home/wayland/gtk.nix
+++ home/wayland/gtk.nix
@@ -29,15 +29,11 @@
     };
 
     gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
     };
 
     gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
     };
   };
   home.sessionVariables.GTK_THEME = "RosePine-Main-B-LB";
